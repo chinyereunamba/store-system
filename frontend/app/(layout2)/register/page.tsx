d467eaf3ce69f3b5f33c4e11a8ccf7a5bd2849d6
Chinyere <chinyereunamba17@gmail.com>
2024-10-04 11:16:21 +0100

    renamed

--- frontend/app/(layout2)/register/page.tsx
+++ frontend/app/(layout2)/register/page.tsx
@@ -2,12 +2,14 @@ import Register from "@/components/auth/Register";
 import React from "react";
 import { FaUserCircle } from "react-icons/fa";
 
+
+
 export default function SignUpPage() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="profile">
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div className="profile self-center">
         <h1>Anol</h1>
-        <FaUserCircle />
+        <FaUserCircle size={35}/>
       </div>
       <Register />
     </div>
--- frontend/components/auth/Register.tsx
+++ frontend/components/auth/Register.tsx
@@ -1,59 +1,102 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { FormEvent, useState } from "react";
 
 export default function Register() {
+  const [user, setUser] = useState({
+    email: "",
+    username: "",
+    password: "",
+  });
+
+  const router = useRouter()
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    const data = {
+      email: user.email,
+      username: user.username,
+      password1: user.password,
+      password2: user.password,
+    };
+
+    const response =  fetch(
+      `http://127.0.0.1:8000/api/user/registration/`, {
+        method: 'post',
+        headers: {
+          'Content-Type':'application/json'
+        },
+        body: JSON.stringify(data)
+      }
+    );
+
+    return router.push('/')
+
+  };
   return (
-    <>
-      <form action="" className="flex flex-col gap-3 w-96">
-        <div className="flex flex-col">
-          <label htmlFor="username">Username</label>
-          <input
-            type="username"
-            name="username"
-            id="username"
-            className="p-3 rounded-md"
-            placeholder="admin"
-          />
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className="p-3 rounded-md"
-            placeholder="admin@gmail.com"
-          />
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            className="p-3 rounded-md"
-            placeholder="*********"
-          />
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="repassword">Re-enter Password</label>
-          <input
-            type="password"
-            name="password"
-            id="repassword"
-            className="p-3 rounded-md"
-            placeholder="**********"
-          />
-        </div>
-        <button type="submit" className="border-2 border-black p-3 rounded-md">
-          Sign up
-        </button>
-        <div className="flex">
-          <p className="capitalize">
-            already have an account? <Link className="hover:underline" href={"/login"}>Login</Link>
-          </p>
-        </div>
-      </form>
-    </>
+    <form action="" className="flex flex-col gap-3 w-96" onSubmit={handleSubmit}>
+      <div className="flex flex-col">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          className="p-3 rounded-md"
+          placeholder="admin@gmail.com"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.currentTarget.value })}
+        />
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="username">Username</label>
+        <input
+          type="username"
+          name="username"
+          id="username"
+          className="p-3 rounded-md"
+          placeholder="admin"
+          value={user.username}
+          onChange={(e) =>
+            setUser({ ...user, username: e.currentTarget.value })
+          }
+        />
+      </div>{" "}
+      <div className="flex flex-col">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          className="p-3 rounded-md"
+          placeholder="*********"
+          value={user.password}
+          onChange={(e) =>
+            setUser({ ...user, password: e.currentTarget.value })
+          }
+        />
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="repassword">Re-enter Password</label>
+        <input
+          type="password"
+          name="password"
+          id="repassword"
+          className="p-3 rounded-md"
+          placeholder="**********"
+        />
+      </div>
+      <button type="submit" className="border-2 border-black p-3 rounded-md">
+        Sign up
+      </button>
+      <div className="flex">
+        <p className="capitalize">
+          already have an account?{" "}
+          <Link className="hover:underline" href={"/login"}>
+            Login
+          </Link>
+        </p>
+      </div>
+    </form>
   );
 }
